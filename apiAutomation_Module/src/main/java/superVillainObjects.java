d5f3e458e209eb343bdbb5ce7d4361f10df306e2
Ardra Ashok <77973256+myWorksPiya@users.noreply.github.com>
2022-03-17 12:31:46 +1100

    Update superVillainObjects.java

--- apiAutomation_Module/src/main/java/superVillainObjects.java
+++ apiAutomation_Module/src/main/java/superVillainObjects.java
@@ -2,7 +2,7 @@ public class superVillainObjects {
 
     public static String token = "eyJhbGciOiJIUzUxMiIsInR5cCI6IkpXVCJ9.eyJrZXkiOiJhYWRyYSIsImVtYWlsIjoicGl5YWFzb2tAZ21haWwuY29tIiwiaWF0IjoxNjQ0MzYyMjM0LCJleHAiOjE2NDQ2MjE0MzR9.auoTpLRY1-JE_rfI34SltoWqOj4Mbo280PBRTb_SjZSY7wK_tMZPlfeLv-IQyJU6CEBpPqlQjUDZeEqxUchMQg";
 
-    public  static String body_string = "{\n" +
+    public  static String body_string = "{\n hello" +
             "  \"username\": \"Heaven\",\n" +
             "  \"score\": 500\n" +
             "}";
